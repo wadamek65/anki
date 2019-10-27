864b462c2d3c4cb5c6c5e08f5f2e9cea9878cb85
wadamek65 <wadamek65@gmail.com>
2019-10-27 17:28:56 +0100

    Change backend language from python to node

---
+++ backend/index.ts
@@ -0,0 +1,8 @@
+import * as mongoose  from 'mongoose';
+import { server } from './src/apollo';
+
+(async () => await mongoose.connect('mongodb://localhost/ll', { useNewUrlParser: true, useUnifiedTopology: true }))();
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`)
+}).catch(err => console.error(err));
--- backend/src/graphql/__init__.py
+++
@@ -1,5 +0,0 @@
-import graphene
-
-from src.graphql import mutation, query
-
-SCHEMA = graphene.Schema(query=query.Query, mutation=mutation.Mutations, types=[query.Vocabulary, query.Word])
