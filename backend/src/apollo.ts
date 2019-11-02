83dad0c0fc394e01d72904632ed89cc565848d7c
wadamek65 <wadamek65@gmail.com>
2019-11-03 00:04:37 +0100

    Add google auth middleware validation to apollo context

--- backend/src/apollo.ts
+++ backend/src/apollo.ts
@@ -1,4 +1,5 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, AuthenticationError } from 'apollo-server';
+import { OAuth2Client } from 'google-auth-library';
 import { importSchema } from 'graphql-import';
 
 import { Mutation } from './mutation';
@@ -22,4 +23,22 @@ const resolvers = {
 	Mutation
 };
 const typeDefs = importSchema('../schema.graphql');
-export const server = new ApolloServer({ typeDefs, resolvers });
+
+const CLIENT_ID = '561207995036-g3g2jjd7af267q2q7vfb8tdqhahkn0f8.apps.googleusercontent.com';
+const authClient = new OAuth2Client(CLIENT_ID);
+
+export const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: async ({ req }) => {
+		try {
+			const ticket = await authClient.verifyIdToken({
+				idToken: req.headers['x-access-token'] as string,
+				audience: CLIENT_ID
+			});
+			return ticket.getPayload();
+		} catch {
+			throw new AuthenticationError('Unauthenticated.');
+		}
+	}
+});
