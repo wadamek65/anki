7c2f971c33e5ffd084156dc987f5bcebeeff718d
wadamek65 <wadamek65@gmail.com>
2019-11-02 14:14:55 +0100

    Add app theme and grid styles for mobile

---
+++ frontend/src/lib/styles.ts
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+
+export const mobile = (styles: string) => css`
+    @media only screen and (max-width: 500px) { ${styles} }
+`;
+
+export const grid = css`
+		display: grid;
+		${mobile(`
+			grid-column-gap: 16px;
+			grid-template-columns: 1fr 1fr 1fr 1fr;
+			grid-template-rows: auto;
+			margin: 0 16px;
+		`)}
+	`;
---
+++ frontend/src/lib/theme.ts
@@ -0,0 +1,44 @@
+
+export const theme = {
+	navHeight: '50px',
+	color: {
+		background: '#F3F3F3',
+		primary: {
+			dark: '#3660CC',
+			neutral: '#889BFF',
+			light: '#ADC4FF',
+			white: '#CFDDFF',
+			contrast: '#ffffff'
+		},
+		gray: {
+			normal: '#7A7A7A'
+		}
+	},
+	font: {
+		weight: {
+			extraLight: 100,
+			thin: 200,
+			light: 300,
+			regular: 400,
+			medium: 500,
+			semiBold: 600,
+			bold: 700,
+			extraBold: 800,
+			black: 900
+		},
+		size: {
+			small: '12px',
+			regular: '16px',
+			medium: '18px',
+			title: '28px'
+		}
+	},
+	spacing: {
+		tiny: '4px',
+		small: '8px',
+		medium: '16px',
+		large: '24px',
+		veryLarge: '32px',
+		huge: '40px'
+	}
+};
