db2b09da535bdb6c75670c3aa51760035ecba7c7
wadamek65 <wadamek65@gmail.com>
2019-11-03 00:17:07 +0100

    Add hook to include token header in all requests

--- frontend/src/lib/relay.ts
+++ frontend/src/lib/relay.ts
@@ -1,8 +1,21 @@
-import ky from 'ky';
+import ky, { BeforeRequestHook } from 'ky';
 import { Environment, GraphQLResponse, Network, RecordSource, Store } from 'relay-runtime';
 
+const addAuthHeader: BeforeRequestHook = (input, options) => {
+	const clientId = localStorage.getItem('user');
+	if (clientId) {
+		options.headers.append('x-access-token', clientId);
+	}
+};
+
+const client = ky.create({
+	hooks: {
+		beforeRequest: [addAuthHeader]
+	}
+});
+
 const fetchQuery = (operation: any, variables: any) =>
-	ky
+	client
 		.post('http://localhost:4000/graphql', {
 			json: {
 				query: operation.text,
