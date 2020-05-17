aef0536203cd53a4a9b2f583154ea2b53e87e2fb
wadamek65 <wadamek65@gmail.com>
2020-05-17 20:10:23 +0200

    Fix disable auth on feature branches

--- frontend/src/views/Main/Main.tsx
+++ frontend/src/views/Main/Main.tsx
@@ -4,11 +4,12 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { ErrorBoundary } from '../../components/ErrorBoundary';
 import { REFRESH_TOKEN_KEY } from '../Login';
 import { MobileNavbar } from './components/Navbar';
+import { CLIENT_ID } from '../../lib/config';
 
 export const Main: React.FC = () => {
 	const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY);
 
-	if (refreshToken === null) {
+	if (refreshToken === null && CLIENT_ID !== '') {
 		return <Navigate to="/login" />;
 	}
 
